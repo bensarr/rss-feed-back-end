d6168dab6a08ad53a7d449af1c9380cd5528951c
bensarr <bensarr37@gamil.com>
2022-04-23 11:08:46 +0000

    Implement Back-End Article DataBase Crud

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     HOST: "ec2-3-229-252-6.compute-1.amazonaws.com",
+    PORT: "5432",
     USER: "hdosisihohemzn",
     PASSWORD: "952f8158beca6f953d671b8ad8437937a1cbae736c1eab7ea5edd1cada7cb2a1",
     DB: "d3u9k5m5qunfr9",
